ca46aeaa47d0b10e08bead0a28622dbff2440ef6
JaepaL <104006620+DevJaepaL@users.noreply.github.com>
2022-06-26 02:14:49 +0900

    22-06-29(간단한 코드 추가)

--- C/구조체 (Structure)/Structure.c
+++ C/구조체 (Structure)/Structure.c
@@ -1,5 +1,26 @@
+/* === 구조체 변수를 선언하는 동시에 초기화하기. === */
 #include <stdio.h>
 
-int main(void){
+struct gameID {
+    char userName[20];
+    char userJob[10];
+    int userLevel;
+};
+
+int main(void)
+{
     
+    struct Person user_01 = { .userName = "JaepaL", .userJob = "마법사", .userLevel = 10 };
+
+    printf("유저 네임 : %s\n", user_01.userName);       // 유저 네임 : JaeapL
+    printf("유저 직업 : %d\n", user_01.userJob);        // 유저 직업 : 마법사
+    printf("유저 레벨 : %s\n", user_01.userLevel);    // 유저 레벨 : 10
+
+    struct Person user_02 = { "GameManager", "SuperUser" , "100" };
+
+    printf("유저 네임 : %s\n", user_02.userName);       // 유저 네임 : GameManager
+    printf("유저 직업 : %d\n", user_02.userJob);        // 유저 직업 : SuperUser
+    printf("유저 레벨 : %s\n", user_02.userLevel);    // 유저 레벨 : 100
+
+    return 0;
 }
