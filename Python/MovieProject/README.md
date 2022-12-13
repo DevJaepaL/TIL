1e272d27a114ad3deeafea8aaf33ebabf590da9b
JaepaL <104006620+DevJaepaL@users.noreply.github.com>
2022-12-13 13:49:07 +0900

    ::: Python :::
    파이썬 프로젝트 완료

--- Python/MovieProject/README.md
+++ Python/MovieProject/README.md
@@ -1,11 +1,78 @@
-## 영화별 리뷰 분석
+# 영화별 리뷰 분석
 
 `movie_aisw.csv` 파일 이용하여 다음과 같은 사항을 작성한다.
 
-1. 파일 읽어오기
+1. 파일 읽어오기(`movie_aisw.csv`)
 2. 데이터 탐색(구성과 데이터 타입 확인)
 3. 영화 개수 확인
 4. 영화별 평점과 리뷰 수 확인
 5. '올빼미' 영화에 대한 형태소 분석(불용어 제거)
 6. '올빼미' 영화에 대한 `Wordcloud` 생성
 7. 영화 리뷰수가 두번 째로 많은 영화의 분석 및 워드클라우드 생성(1-6번 반복)
+
+
+## 1. 파일 읽어오기
+
+```python
+# 필요 라이브러리 로드
+import pandas as pd
+import numpy as np
+import collections
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+from konlpy.tag import Okt
+
+# 1. 파일 읽어오기
+df = pd.read_csv('Python/MovieProject/data/movie_aisw.csv')
+```
+해당 사항을 작성하기 위한 라이브러리들을 불러온다.
+그 후, 주어진 csv 파일을 `pandas`를 이용해 로드한다.
+
+## 2. 데이터 탐색(구성과 데이터 타입 확인)
+
+```python
+# 2. 데이터 탐색(구성과 데이터 타입 확인)
+print(df.info())
+print(df.dtypes)
+```
+로드한 데이터의 정보들의 구성과 데이터 타입을 확인하기 위해 `info()` 함수와 `dtypes` 를 사용한다.
+
+### 결과
+
+```python
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 4139 entries, 0 to 4138
+Data columns (total 3 columns):
+ #   Column    Non-Null Count  Dtype
+---  ------    --------------  -----
+ 0   movie     4139 non-null   object
+ 1   sentence  4139 non-null   object
+ 2   score     4139 non-null   int64
+dtypes: int64(1), object(2)
+memory usage: 97.1+ KB
+None
+movie       object
+sentence    object
+score        int64
+dtype: object
+```
+
+## 3. 영화 개수 확인
+
+```python
+# 3. 영화 개수 확인
+print(df['movie'].describe())
+print(df['movie'].unique()) # 중복 없이 나온 영화 제목 : 총 6개
+```
+
+### 결과
+```python
+count     4139
+unique       6
+top        올빼미
+freq      2994
+Name: movie, dtype: object
+
+['올빼미' '동감' '유포자들' '블랙 팬서: 와칸다 포에버' '스트레인지 월드' '데시벨']
+```
+`movie` 컬럼의 정보를 확인하기 위해 `descrbie()` 함수를 이용해서 정보를 확인했다. 알 수 있는 정보로는 총 개수(4139개)와 중복 값을 제거한 개수(6개), 제일 많이 사용한 데이터(`올빼미`)와 빈도수(2994개)를 알수 있다.
--- Python/MovieProject/ReviewAnalysis.py
+++ Python/MovieProject/ReviewAnalysis.py
@@ -1,11 +1,65 @@
 # 필요 라이브러리 로드
 import pandas as pd
+import numpy as np
+import collections
 import matplotlib.pyplot as plt
-from wordcloud import wordcloud
+from wordcloud import WordCloud
 from konlpy.tag import Okt
 
 # 1. 파일 읽어오기
-df = pd.read_table('Python\MovieProject\data\movie_aisw.csv')
-print(df)
-df.info()
+df = pd.read_csv('Python/MovieProject/data/movie_aisw.csv')
+
+# 2. 데이터 탐색(구성과 데이터 타입 확인)
+print(df.info())
 print(df.dtypes)
+
+# 3. 영화 개수 확인
+print(df['movie'].describe())
+print(df['movie'].unique()) # 중복 없이 나온 영화 제목 : 총 6개
+
+# 4. 영화별 평점과 리뷰 수 확인
+# 데이터 프레임의 GroupBy 함수를 이용해 각 행에 영화의 이름별로 그룹화 하여
+# 리뷰 및 평점을 영화 별 펑점과 리뷰 총 개수 출력
+# movieGroupBy = df.groupby('movie')
+# print(movieGroupBy.count())
+
+# 5. '올빼미' 영화에 대한 형태소 분석(불용어 제거)
+ # 원본 데이터프레임에서 'movie' 컬럼의 '올빼미'가 포함된 행만 추출하여 
+ # 새로운 데이터 프레임인 `df_owl` 생성.
+# df_owl = df[df['movie'].str.contains('올빼미')].copy()
+# print(df_owl.count())
+# # 원본 데이터 수(df) : 4139
+# # 올빼미 영화의 데이터(df_owl) : 2994
+
+# # 중복 제거 및 결측치 확인
+# print(df_owl['movie'].nunique())
+# print(df_owl.isnull().sum()) # 결측치와 중복값 없는것을 확인
+
+# # 불용어 제거 및 형태소 토큰화
+# okt = Okt()
+# stopword = ['점', '정말', '왜', '말', '그', '없다', 
+#             '정도', '걸', '뭐', '이건','영화', '완전', 
+#             '좀', '있는', '거', '나','이', '볼', '입니다', 
+#             '것', '이런', '더', '수', '때']
+# list = []
+# for sentence in df_owl['sentence']:
+#     s_list = okt.pos(sentence)
+#     for word, tag in s_list:
+#         if word not in stopword:
+#             if tag in ['Noun', 'Adjective']:
+#                 list.append((word))
+
+# counts = collections.Counter(list)
+# tag = counts.most_common(50)
+# # 빈도수가 많은 상위 50개의 단어 출력
+# print(tag)
+
+# # 6. '올빼미' 영화에 대한 WordCloud 생성
+# font_path = 'C:\Windows\Fonts\malgun.ttf'
+# wc = WordCloud(font_path=font_path, background_color='black', max_font_size=50)
+
+# # 빈도수가 많은 상위 50개의 단어로 워드클라우드 생성
+# cloud = wc.generate_from_frequencies(dict(tag))
+# # 화면에 출력
+# plt.imshow(cloud)
+# plt.show()
