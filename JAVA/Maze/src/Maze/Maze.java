d961108705d4afbabfd66a9ac4bed606e2678d01
JaepaL <104006620+DevJaepaL@users.noreply.github.com>
2023-03-27 15:53:07 +0900

    <ADD> : 행렬 알고리즘 프로젝트 추가 🔼

---
+++ JAVA/Maze/src/Maze/Maze.java
@@ -0,0 +1,7 @@
+package Maze;
+
+public class Maze {
+    private int[][] matrix = {{0,0,0},
+                              {1,0,0},
+                              {0,1,2}};
+}
---
+++ JAVA/Maze/src/Maze/Program.java
@@ -0,0 +1,7 @@
+package Maze;
+
+public class Program {
+    public static void main(String[] args) {
+        
+    }
+}
